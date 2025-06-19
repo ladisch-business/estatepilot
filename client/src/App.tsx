ddcc88f408d59c968489a670806871677c1087a8
ladisch-business <dev@ladisch-business.de>
2025-06-19 23:03:15 +0200

    erste Strukturen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* ./client/src/App.tsx */
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return (
+        <div className="bg-slate-900 text-white min-h-screen flex items-center justify-center">
+            <h1 className="text-4xl font-bold">Unser Projekt startet hier!</h1>
+        </div>
+    );
 }
-
-export default App
